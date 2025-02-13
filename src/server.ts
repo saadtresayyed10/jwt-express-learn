ebf1a08bc4bdb2d329fd23b687eb5bdfc2dcbf73
unknown <saadsyed950@gmail.com>
2025-02-13 12:59:21 +0530

    Server setup complete

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("Hey now");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Server is running!</h1>");
+});
+
+app.listen(PORT, () => console.log("Server is running..."));
