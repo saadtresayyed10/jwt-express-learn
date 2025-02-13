b652c2e0309957e785cf1cdc31105f98136c9af3
unknown <saadsyed950@gmail.com>
2025-02-13 13:54:54 +0530

    Services are done

--- src/service/user-service/user.service.ts
+++ src/service/user-service/user.service.ts
@@ -25,3 +25,21 @@ export const registerUser = async (
     },
   });
 };
+
+export const loginUser = async (email: string, password: string) => {
+  const user = await prisma.users.findUnique({ where: { email } });
+  if (!user) throw new Error("User with this email do not exists");
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) throw new Error("Password incorrect");
+
+  const token = generateToken(user.id);
+  return { token, user };
+};
+
+export const userProfile = async (userId: string) => {
+  return await prisma.users.findUnique({
+    where: { id: userId },
+    select: { email: true, name: true, phone: true },
+  });
+};
